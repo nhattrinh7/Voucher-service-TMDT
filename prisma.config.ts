7053a474b25584097df7c53b27ff5246ab2c049e
Trịnh Minh Nhật <trinhminhnhatym@gmail.com>
2026-04-07 22:40:02 +0700

    fix envPath trong prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig, env } from 'prisma/config'
-import 'dotenv/config'
+import { config } from 'dotenv'
+
+const envPath =
+  process.env.DOTENV_CONFIG_PATH ??
+  (process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development')
+
+config({ path: envPath })
 
 export default defineConfig({
   schema: 'src/infrastructure/database/prisma/schema.prisma',
